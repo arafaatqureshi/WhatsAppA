839ef8fb8d6550c18dd14abd618892c822446834
arafaatqureshi <arafaatqureshi@gmail.com>
2020-02-04 10:34:09 +0500

    second

--- app/src/main/java/com/example/whatsapp/MainActivity.kt
+++ app/src/main/java/com/example/whatsapp/MainActivity.kt
@@ -14,17 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val vp = findViewById<ViewPager>(R.id.viewPager)
-    val Adapter= Adapter(supportFragmentManager)
+        val Adapter= Adapter(supportFragmentManager)
         Adapter.addFragment(fragment1(),"Chats")
         Adapter.addFragment(fragmentStatus(),"Status")
         Adapter.addFragment(fragmentCalls(),"Calls")
         vp.adapter= Adapter
         tabs.setupWithViewPager(vp)
 
-     var helper :helperMethod =helperMethod()
+        var helper :helperMethod =helperMethod()
         var a = helper.name
 
-//            Toast.makeText(this,"myLibrary",Toast.LENGTH_SHORT).show()
+        
         println("\n \n \n"+a);
 
     }
